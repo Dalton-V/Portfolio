539e1fc02f6ef371b7940bfc71fa653599cd89c1
Dalton-V <daltonvislisel@gmail.com>
2025-08-07 23:31:51 -0500

    Added Theme Toggle and Background CSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return <div className="min-h-dvh bg-background text-foreground overflow-x-hidden">
+        
+        {/* Theme Toggle */}
+        <ThemeToggle/>
+        {/* Backgound Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+    </div>
+};
