8ed411d75259526e310dc849b003acdc24f56974
Dalton-V <dalton216@gmail.com>
2025-08-08 18:27:26 -0500

    Fixed clsx inputs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
     return <div className="min-h-dvh bg-background text-foreground overflow-x-hidden">
@@ -13,5 +14,8 @@ export const Home = () => {
         {/* Navbar */}
         <Navbar />
         {/* Main Content */}
+        <main>
+            <HeroSection/>
+        </main>
     </div>
 };
