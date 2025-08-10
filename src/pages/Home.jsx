2d7985bb1cf4eefcf0c19765420a81d3cd15a7a0
Dalton-V <dalton216@gmail.com>
2025-08-09 21:46:17 -0500

    Added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
     return <div className="min-h-dvh bg-background text-foreground overflow-x-hidden">
@@ -18,6 +19,7 @@ export const Home = () => {
         <main>
             <HeroSection/>
             <AboutSection/>
+            <SkillsSection/>
         </main>
     </div>
 };
