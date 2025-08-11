e572b9e8b976d2514d047eb3e55f3231407658b7
Dalton-V <dalton216@gmail.com>
2025-08-10 20:09:25 -0500

    Added footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return <div className="min-h-dvh bg-background text-foreground overflow-x-hidden">
@@ -25,5 +26,7 @@ export const Home = () => {
             <ProjectSection/>
             <ContactSection/>
         </main>
+
+        <Footer/>
     </div>
 };
